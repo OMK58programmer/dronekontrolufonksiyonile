5037a34cb0746dad33713ba364047b0f94f94a0e
OMK58programmer <kocosmanmelih@gmail.com>
2025-11-24 20:22:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,40 @@
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-string ucusGuvenligiKontrol(string droneIsmi, int pilSeviyesi, int yuk, int hiz, int yukseklik) {
+string ucusGuvenligiKontrol(string droneIsmi, int yuk, int hiz, int yukseklik) {
+    int consumed = (hiz / 10) * 5;
+    int pil = 100 - consumed;
+    if (pil < 0) pil = 0;
+
+    // Ekrana tüm değerleri yazdır
     cout << "Drone: " << droneIsmi << endl;
-    cout << "Pil Seviyesi: " << pilSeviyesi << endl;
+    cout << "Pil Seviyesi: " << pil << endl;
     cout << "Yük: " << yuk << endl;
     cout << "Hız: " << hiz << endl;
     cout << "Yükseklik: " << yukseklik << endl;
     if (hiz > 10) {
-        cout << " Hız sınırı aşıldı!" << endl;
+        cout << "Uyarı: Hız sınırı aşıldı!" << endl;
     }
 
     if (yuk > 80) {
         return "Ağır yük";
     }
-    if (pilSeviyesi < 30) {
+    if (pil < 30) {
         return "Pil seviyesi düşük";
     }
     if (yukseklik < 50) {
         return "Radar dışı";
     }
     if (hiz > 10) {
-        return "Hız sınırı aşıldı";
+        return "Hız sınırı aşılmış";
     }
-    return "Uçuş güvenli";
+    return "Uçuş güvenli!";
 }
 
 int main() {
-    int pil1 = 60;
-    int pil2 = 25;
-    int pil3 = 55;
-
-    cout << ucusGuvenligiKontrol("drone1", pil1, 85, 11, 50) << endl << endl;   
-    cout << ucusGuvenligiKontrol("drone2", pil2, 50, 10, 3) << endl << endl;    
-    cout << ucusGuvenligiKontrol("drone3", pil3, 20, 15, 40) << endl << endl;   
+    cout << ucusGuvenligiKontrol("drone1", 85, 11, 50) << endl << endl;   
+    cout << ucusGuvenligiKontrol("drone2", 50, 10, 3) << endl << endl;    
+    cout << ucusGuvenligiKontrol("drone3", 20, 15, 40) << endl << endl;   
     return 0;
 }
